26feaf1671f7a88d2024aacbfe59eddb2e2956ea
ಜಗದೀಶ ಎನ್ Jagadish N <jkanasu@users.noreply.github.com>
2021-09-19 12:53:59 -0700

    Tool to get winds and temp data
    Simple tool to get winds and temp data from aviationweather.gov

---
+++ vaayuyaana/getwinds.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# In the for loop select appropriately
+for region in sfo #pac bos mia chi dfw slc sfo alaska hawaii other_pac
+do
+    for fcst in 06 12 24
+    do
+        for level in low #high
+        do
+            echo Processing region ${region} for forecast ${fcst} for level ${level}
+            rawfile=winds_${region}_${fcst}_${level}.out
+            echo curl -s 'https://aviationweather.gov/windtemp/data?level='${level}'&fcst='${fcst}'&region='${region}'&layout=off&date=' -o ${rawfile}
+            curl -s 'https://aviationweather.gov/windtemp/data?level='${level}'&fcst='${fcst}'&region='${region}'&layout=off&date=' -o ${rawfile}
+            csvfilename=${region}_${fcst}_${level}_`cat ${rawfile} | grep -v -E '<|>|"|{|}' | grep -E 'BASED|VALID' | xargs echo | awk '{print "BASED_"$4"_VALID_"$6"_FORUSE_"$9"csv"}'`
+            cat ${rawfile} | grep -v -E 'BASED|VALID|FD.US.|^000|^FB.' | grep -v -E '<|>|"|{|}' | sed 's/^\(.\{3\}\)./\1,/' | sed 's/^\(.\{8\}\)./\1,/' | sed 's/^\(.\{16\}\)./\1,/' | sed 's/^\(.\{24\}\)./\1,/' | sed 's/^\(.\{32\}\)./\1,/' | sed 's/^\(.\{40\}\)./\1,/' | sed 's/^\(.\{48\}\)./\1,/' | sed 's/^\(.\{55\}\)./\1,/' | sed 's/^\(.\{62\}\)./\1,/' | grep -v '^$' > ${csvfilename}
+        done
+    done
+done
