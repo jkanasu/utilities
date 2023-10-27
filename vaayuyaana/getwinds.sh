a3ed812e37e7e81056ff94744f711f668a3b1eb6
ಜಗದೀಶ ಎನ್ Jagadish N <jkanasu@users.noreply.github.com>
2023-10-27 16:25:34 -0700

    adapt to aviationweather website
    The aviation weather website changed and with it the api endpoints as well

--- vaayuyaana/getwinds.sh
+++ vaayuyaana/getwinds.sh
@@ -8,10 +8,10 @@ do
         do
             echo Processing region ${region} for forecast ${fcst} for level ${level}
             rawfile=winds_${region}_${fcst}_${level}.out
-            echo curl -s 'https://aviationweather.gov/windtemp/data?level='${level}'&fcst='${fcst}'&region='${region}'&layout=off&date=' -o ${rawfile}
-            curl -s 'https://aviationweather.gov/windtemp/data?level='${level}'&fcst='${fcst}'&region='${region}'&layout=off&date=' -o ${rawfile}
+            echo curl -s 'https://aviationweather.gov/api/data/windtemp?level='${level}'&fcst='${fcst}'&region='${region}'&layout=off&date=' -o ${rawfile}
+            curl -s 'https://aviationweather.gov/api/data/windtemp?level='${level}'&fcst='${fcst}'&region='${region}'&layout=off&date=' -o ${rawfile}
             csvfilename=${region}_${fcst}_${level}_`cat ${rawfile} | grep -v -E '<|>|"|{|}' | grep -E 'BASED|VALID' | xargs echo | awk '{print "BASED_"$4"_VALID_"$6"_FORUSE_"$9"csv"}'`
-            cat ${rawfile} | grep -v -E 'BASED|VALID|FD.US.|^000|^FB.' | grep -v -E '<|>|"|{|}' | sed 's/^\(.\{3\}\)./\1,/' | sed 's/^\(.\{8\}\)./\1,/' | sed 's/^\(.\{16\}\)./\1,/' | sed 's/^\(.\{24\}\)./\1,/' | sed 's/^\(.\{32\}\)./\1,/' | sed 's/^\(.\{40\}\)./\1,/' | sed 's/^\(.\{48\}\)./\1,/' | sed 's/^\(.\{55\}\)./\1,/' | sed 's/^\(.\{62\}\)./\1,/' | grep -v '^$' > ${csvfilename}
+            cat ${rawfile} | grep -v -E 'Extracted|BASED|VALID|FD.US.|^000|^FB.' | grep -v -E '<|>|"|{|}' | sed 's/^\(.\{3\}\)./\1,/' | sed 's/^\(.\{8\}\)./\1,/' | sed 's/^\(.\{16\}\)./\1,/' | sed 's/^\(.\{24\}\)./\1,/' | sed 's/^\(.\{32\}\)./\1,/' | sed 's/^\(.\{40\}\)./\1,/' | sed 's/^\(.\{48\}\)./\1,/' | sed 's/^\(.\{55\}\)./\1,/' | sed 's/^\(.\{62\}\)./\1,/' | grep -v '^$' > ${csvfilename}
         done
     done
 done
